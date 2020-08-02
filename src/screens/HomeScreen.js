987fa195b191b86f0aa94914016d266e89cc511e
Azhar7909 <muhammadazharhussain54@gmail.com>
2020-08-02 11:22:21 +0500

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,6 +4,7 @@ export default function HomeScreen() {
     return (
         <div style={{textAlign:'center', color: 'darkcyan'}}>
             <h1>Home Screen</h1>
+            <h3>Click on Product <br/>(To see Items)</h3>
         </div>
     )
 }
